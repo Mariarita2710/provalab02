ef282efb7c28d5629de0d6e94b918ecfd40b30c4
fraservente00 <francescoservente@gmail.com>
2025-04-06 22:19:54 +0200

    fix: fixing some errors

--- ReservationService/config/SpringConfiguration.kt
+++ ReservationService/config/SpringConfiguration.kt
@@ -1,34 +1,21 @@
-package org.example.reservationservice.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.Resource
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import org.springframework.web.servlet.resource.PathResourceResolver
-import org.springframework.web.servlet.resource.ResourceResolverChain
-import jakarta.servlet.http.HttpServletRequest
-
 @Configuration
 class SpringConfiguration : WebMvcConfigurer {
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         serveDirectory(registry, "/ui", "classpath:/ui/")
     }
-
     private fun serveDirectory(registry: ResourceHandlerRegistry, endpoint: String, location: String) {
         val baseEndpoint = endpoint.trimEnd('/')
         val endpointPatterns = listOf(
             baseEndpoint,
-            "$baseEndpoint/",
-            "$baseEndpoint/**"
+            “$baseEndpoint/”,
+        "$baseEndpoint/**"
         ).toTypedArray()
-
         registry.addResourceHandler(*endpointPatterns)
             .addResourceLocations("${location.trimEnd('/')}/")
             .resourceChain(false)
             .addResolver(CustomPathResourceResolver())
     }
 }
-
 class CustomPathResourceResolver : PathResourceResolver() {
     override fun resolveResource(
         request: HttpServletRequest?,
--- reservation-frontend/vite.config.ts
+++ reservation-frontend/vite.config.ts
@@ -2,16 +2,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Rende il server accessibile da altri dispositivi
-    port: 5173, // Facoltativo, per specificare la porta
-  },
   base: '/ui/', // 👈 importante per routing corretto in produzione
   build: {
     emptyOutDir: true,
-    outDir: '../ReservationService/src/main/resources/ui', // 👈 build direttamente nel backend
+    outDir: '../ReservationService/src/main/resources/ui', //  build direttamente nel backend
   },
 })
