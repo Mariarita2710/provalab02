328c529a06629ffee5ff8d00c7356b10531806f6
fraservente00 <francescoservente@gmail.com>
2025-04-07 15:44:00 +0200

    fix: fixing some errors

--- ReservationService/src/main/kotlin/org/example/reservationservice/config/SpringConfiguration.kt
+++ ReservationService/src/main/kotlin/org/example/reservationservice/config/SpringConfiguration.kt
@@ -1,21 +1,34 @@
+package org.example.reservationservice.config
+
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.Resource
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.resource.PathResourceResolver
+import org.springframework.web.servlet.resource.ResourceResolverChain
+
 @Configuration
 class SpringConfiguration : WebMvcConfigurer {
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        serveDirectory(registry, "/ui", "classpath:/ui/")
+        serveDirectory(registry, "/ui", "classpath:/ui")
     }
+
     private fun serveDirectory(registry: ResourceHandlerRegistry, endpoint: String, location: String) {
         val baseEndpoint = endpoint.trimEnd('/')
         val endpointPatterns = listOf(
             baseEndpoint,
-            “$baseEndpoint/”,
-        "$baseEndpoint/**"
+            "$baseEndpoint/",
+            "$baseEndpoint/**"
         ).toTypedArray()
+
         registry.addResourceHandler(*endpointPatterns)
             .addResourceLocations("${location.trimEnd('/')}/")
             .resourceChain(false)
             .addResolver(CustomPathResourceResolver())
     }
 }
+
 class CustomPathResourceResolver : PathResourceResolver() {
     override fun resolveResource(
         request: HttpServletRequest?,
--- reservation-frontend/vite.config.ts
+++ reservation-frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ui/', // 👈 importante per routing corretto in produzione
+  base: '/ui/',
   build: {
     emptyOutDir: true,
     outDir: '../ReservationService/src/main/resources/ui', //  build direttamente nel backend
