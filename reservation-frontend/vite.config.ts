32f13ac902c924589e6da75b53a838028fe48889
fraservente00 <francescoservente@gmail.com>
2025-04-07 15:59:22 +0200

    fix: fixing some errors

--- reservation-frontend/vite.config.ts
+++ reservation-frontend/vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   base: '/ui/',
   build: {
-    emptyOutDir: true,
-    outDir: '../ReservationService/src/main/resources/ui', //  build direttamente nel backend
-  },
+    outDir: '../ReservationService/src/main/resources/ui',
+    emptyOutDir: true
+  }
 })
